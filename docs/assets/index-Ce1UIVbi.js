c2ea78067dd3ced15f4fa2af4a52e4dd5257d896
JerryCoal <203106887@qq.com>
2026-09-13 10:03:27 +0800

    Publish static Shengzhang Studio with encrypted local user workspaces

---
+++ docs/assets/index-Ce1UIVbi.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-galP2_6a.js","./index-BT-qTb8n.js","./index-BrQvzM67.css"])))=>i.map(i=>d[i]);
+import{e as r,_ as t}from"./index-BT-qTb8n.js";const _=r("Share",{web:()=>t(()=>import("./web-galP2_6a.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
